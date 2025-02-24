66de4f4414f5e8703ae2627777788c66d3a77807
lxx01110 <147406052+lxx01110@users.noreply.github.com>
2025-02-25 00:35:32 +0800

    Update MSD.sh

--- MSD.sh
+++ MSD.sh
@@ -1,5 +1,5 @@
-cd MSD
+# cd MSD
 
-python opt_str.py --dataset amazon   --ptb_type add --rate 0.3 --p 0.4 --r 3 --T 9 --seed 0 1 2 3 4 --device cuda:5 
-python opt_str.py --dataset tfinance --ptb_type add --rate 0.3 --p 0.4 --r 2 --T 3 --seed 0 1 2 3 4 --device cuda:7
-python opt_str.py --dataset yelp     --ptb_type add --rate 0.3 --p 0.6 --r 9 --T 5 --seed 0 1 2 3 4 --device cuda:6
+python MSD/opt_str.py --dataset amazon   --ptb_type add --rate 0.3 --p 0.4 --r 3 --T 9 --seed 0 1 2 3 4 --device cuda:5 
+python MSD/opt_str.py --dataset tfinance --ptb_type add --rate 0.3 --p 0.4 --r 2 --T 3 --seed 0 1 2 3 4 --device cuda:7
+python MSD/opt_str.py --dataset yelp     --ptb_type add --rate 0.3 --p 0.6 --r 9 --T 5 --seed 0 1 2 3 4 --device cuda:6
