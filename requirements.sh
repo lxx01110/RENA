2ed3dc451a03b806801a98f66ef45d0e5d163ee5
lxx01110 <147406052+lxx01110@users.noreply.github.com>
2025-02-25 00:47:41 +0800

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -1,1 +1,7 @@
 
+pip install torch==2.4
+pip install torch-geometric==2.6.1
+pip install numpy==1.24
+pip install scikit-learn
+pip install dgl -f https://data.dgl.ai/wheels/torch-2.4/cu121/repo.html
+pip install torchdata==0.7.0
