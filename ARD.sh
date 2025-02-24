ecbb5195c4e05efdbcbe8fb25f4fc8ee9953bacf
lxx01110 <147406052+lxx01110@users.noreply.github.com>
2025-02-24 14:44:37 +0800

    Update ARD.sh

--- ARD.sh
+++ ARD.sh
@@ -1,23 +1,3 @@
-#python main.py --rate 0.3 --p 0.2 --observed 4.0 --imputed 1.0 --ptb_type add --num_heads 32 --threshold 1.4 --num_hidden 128 --r 2 --T 3 --device cuda:4 --dataset tfinance
-# python main.py --rate 0.3 --p 0.2 --observed 4.0 --imputed 1.0 --ptb_type add --num_heads 16 --threshold 1.0 --num_hidden 128 --r 3 --T 9 --seed 0 1 2 3 4 --device cuda:5 --dataset amazon 
-# python main.py --rate 0.3 --p 0.2 --observed 4.0 --imputed 1.0 --ptb_type add --num_heads 16 --threshold 1.1 --num_hidden 128 --r 3 --T 9 --seed 0 1 2 3 4 --device cuda:5 --dataset amazon 
-# python main.py --rate 0.3 --p 0.2 --observed 4.0 --imputed 1.0 --ptb_type add --num_heads 16 --threshold 1.2 --num_hidden 128 --r 3 --T 9 --seed 0 1 2 3 4 --device cuda:5 --dataset amazon 
-# python main.py --rate 0.3 --p 0.2 --observed 4.0 --imputed 1.0 --ptb_type add --num_heads 16 --threshold 1.3 --num_hidden 128 --r 3 --T 9 --seed 0 1 2 3 4 --device cuda:5 --dataset amazon 
-# python main.py --rate 0.3 --p 0.2 --observed 4.0 --imputed 1.0 --ptb_type add --num_heads 16 --threshold 1.4 --num_hidden 128 --r 3 --T 9 --seed 0 1 2 3 4 --device cuda:5 --dataset amazon 
-
- 
-
-
-
-python main.py --rate 0.3 --p 0.2 --observed 4.0 --imputed 1.0 --ptb_type add --num_heads 32 --threshold 1.0 --num_hidden 128 --r 2 --T 3 --seed 1 2 3 4 --device cuda:5 --dataset tfinance 
-python main.py --rate 0.3 --p 0.2 --observed 4.0 --imputed 1.0 --ptb_type add --num_heads 32 --threshold 1.1 --num_hidden 128 --r 2 --T 3 --seed 1 2 3 4 --device cuda:5 --dataset tfinance 
-python main.py --rate 0.3 --p 0.2 --observed 4.0 --imputed 1.0 --ptb_type add --num_heads 32 --threshold 1.2 --num_hidden 128 --r 2 --T 3 --seed 1 2 3 4 --device cuda:5 --dataset tfinance 
-python main.py --rate 0.3 --p 0.2 --observed 4.0 --imputed 1.0 --ptb_type add --num_heads 32 --threshold 1.3 --num_hidden 128 --r 2 --T 3 --seed 1 2 3 4 --device cuda:5 --dataset tfinance 
-python main.py --rate 0.3 --p 0.2 --observed 4.0 --imputed 1.0 --ptb_type add --num_heads 32 --threshold 1.4 --num_hidden 128 --r 2 --T 3 --seed 1 2 3 4 --device cuda:5 --dataset tfinance
-
-python main.py --rate 0.3 --p 0.4 --observed 3.0 --imputed 1.0 --ptb_type add --num_heads 16 --threshold 1.0 --num_hidden 256 --r 9 --T 5 --seed 0 1 2 3 4 --device cuda:5 --dataset yelp 
-python main.py --rate 0.3 --p 0.4 --observed 3.0 --imputed 1.0 --ptb_type add --num_heads 16 --threshold 1.02 --num_hidden 256 --r 9 --T 5 --seed 0 1 2 3 4 --device cuda:5 --dataset yelp 
-python main.py --rate 0.3 --p 0.4 --observed 3.0 --imputed 1.0 --ptb_type add --num_heads 16 --threshold 1.05 --num_hidden 256 --r 9 --T 5 --seed 0 1 2 3 4 --device cuda:5 --dataset yelp 
-python main.py --rate 0.3 --p 0.4 --observed 3.0 --imputed 1.0 --ptb_type add --num_heads 16 --threshold 1.1 --num_hidden 256 --r 9 --T 5 --seed 0 1 2 3 4 --device cuda:5 --dataset yelp 
-python main.py --rate 0.3 --p 0.4 --observed 3.0 --imputed 1.0 --ptb_type add --num_heads 16 --threshold 1.2 --num_hidden 256 --r 9 --T 5 --seed 0 1 2 3 4 --device cuda:5 --dataset yelp 
-
+python main.py --rate 0.3 --p 0.4 --observed 2.0 --imputed 1.0 --ptb_type add --num_heads 16 --threshold 1.4 --num_hidden 128 --r 3 --T 9 --device cuda:5 --dataset amazon --loss_fn sce 
+python main.py --rate 0.3 --p 0.4 --observed 4.0 --imputed 1.0 --ptb_type add --num_heads 32 --threshold 1.3 --num_hidden 128 --r 2 --T 3 --device cuda:6 --dataset tfinance --loss_fn sce
+python main.py --rate 0.3 --p 0.6 --observed 3.0 --imputed 1.0 --ptb_type add --num_heads 16 --threshold 1.0 --num_hidden 256 --r 9 --T 5 --device cuda:6 --dataset yelp --loss_fn sce 
